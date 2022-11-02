f7e31c983c0001ee225e0b53604ae322a4f33afc
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-11-02 22:43:30 +0300

    Added color panel

--- src/Components/WorkExamplesBlock.tsx
+++ src/Components/WorkExamplesBlock.tsx
@@ -4,6 +4,7 @@ import {Container} from "../Conteiners/Container";
 import {Work} from "./Work";
 import tickTackToe from "../assets/PHOTOS/tic-tac-toe.png";
 import colorsClone from "../assets/PHOTOS/colors.png"
+import colorsPanel from "../assets/PHOTOS/colorPanel.png"
 
 
 
@@ -14,6 +15,7 @@ export const WorkExamplesBlock: FC = () => {
       <div className={styles.worksContainer}>
         <Work title={"Tick-tack-toe VueJS"} src={tickTackToe} link={"https://uladzislaupapliouka.github.io/tick-tack-toe-vuejs/"}/>
         <Work title={"Colors Clone"} src={colorsClone} link={"https://uladzislaupapliouka.github.io/color-clone/"}/>
+        <Work title={"Color Panel"} src={colorsPanel} link={"https://uladzislaupapliouka.github.io/color-board/"}/>
       </div>
     </Container>
   );
