b8433c473d7ba5e4e6e7f7caa7f80ed01f23f967
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-11-02 22:42:05 +0300

    Added colors-Clone

--- src/Components/WorkExamplesBlock.tsx
+++ src/Components/WorkExamplesBlock.tsx
@@ -3,6 +3,9 @@ import styles from "./WorkExamplesBlock.module.scss";
 import {Container} from "../Conteiners/Container";
 import {Work} from "./Work";
 import tickTackToe from "../assets/PHOTOS/tic-tac-toe.png";
+import colorsClone from "../assets/PHOTOS/colors.png"
+
+
 
 export const WorkExamplesBlock: FC = () => {  
   return (
@@ -10,6 +13,7 @@ export const WorkExamplesBlock: FC = () => {
       <span className={"class-title-span"}>Works</span>
       <div className={styles.worksContainer}>
         <Work title={"Tick-tack-toe VueJS"} src={tickTackToe} link={"https://uladzislaupapliouka.github.io/tick-tack-toe-vuejs/"}/>
+        <Work title={"Colors Clone"} src={colorsClone} link={"https://uladzislaupapliouka.github.io/color-clone/"}/>
       </div>
     </Container>
   );
